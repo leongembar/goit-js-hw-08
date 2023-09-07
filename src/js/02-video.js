b2b8585a42fdde7a3fd708147117c7e91f40ec24
Леонід Гембаровський <leongembar@gmail.com>
2023-09-07 03:34:57 +0300

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_STORAGE_KEY = "videoplayer-current-time";
+
+    player.on('timeupdate', throttle((e) => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, e.seconds);
+      }, 1000)
+    );
+
+    player.setCurrentTime(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,50 @@
 
+import throttle from 'lodash.throttle';
+
+const LOCAL_STORAGE_KEY = "feedback-form-state";
+
+const localItem = new Object();
+
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+}
+
+populateStorage(localItem);
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.input.addEventListener('input', throttle(onInput, 500));
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+
+
+function onFormSubmit(e){
+    e.preventDefault();
+    localStorage.clear(); 
+    console.log(localItem);
+    e.currentTarget.reset();
+}
+
+function  onInput(e){
+ const emailStorage = e.currentTarget.value;
+
+ localItem.email = emailStorage;
+
+ localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(localItem));
+}
+
+function onTextareaInput(e){
+const messageStorage = e.currentTarget.value;
+
+localItem.message = messageStorage;
+
+localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(localItem));
+}
+
+function populateStorage(){
+    localItem = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    refs.input.value = localItem.email;
+    refs.textarea.value = localItem.message;
+}
