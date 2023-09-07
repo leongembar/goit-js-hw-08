2d6a6fc73dadfff4cf35bad995ae62204119358b
Леонід Гембаровський <leongembar@gmail.com>
2023-09-08 02:12:36 +0300

    4

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,35 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
+const containerEl = document.querySelector(".gallery");
 
+function makeStringGallegy(galleryItems) {
+  return galleryItems
+    .map(
+      (item) =>
+        `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img class="gallery__image" 
+                     src="${item.preview}" 
+                     alt="${item.description}" 
+                />
+            </a>
+        </li>
+        `
+    )
+    .join("");
+}
+
+containerEl.innerHTML = makeStringGallegy(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', 
+{
+  captionsData: 'alt',
+  captionDelay:250,
+});
